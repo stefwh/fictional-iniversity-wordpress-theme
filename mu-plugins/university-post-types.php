a61376c8fbfb57d2d2070077c659277d7bce52db
Stefan Haindl <shaindl@anexia-it.com>
2025-04-10 16:52:07 +0200

    Adding events and programs; removing all plugins with gitignore

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,8 +1,12 @@
 <?php
 
 function university_post_types() {
+  
+  // Event Post Type
+
   register_post_type('event', array(
-    'supports' => array('title', 'editor', 'excerpt'),
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt',),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
@@ -15,6 +19,26 @@ function university_post_types() {
       'all_items' => 'All Events',
       'singular_name' => 'Event' // Label used in admin UI
     )
+  ));
+
+  // Program Post Type
+
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'menu_icon' => 'dashicons-calendar-alt', // Icon: https://developer.wordpress.org/resource/dashicons/#calendar-alt
+    'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program' // Label used in admin UI
+    ),
+    'menu_icon' => 'dashicons-awards'
   ));
 }
 
--- themes/fictional-university-theme/single-event.php
+++ themes/fictional-university-theme/single-event.php
@@ -26,6 +26,23 @@
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
+
+    <?php 
+    
+      $relatedPrograms = get_field('related_programs');
+
+      if ($relatedPrograms) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+        echo '<ul class=link-list min-list">';
+        foreach($relatedPrograms as $program) { ?>
+          <li><a href="<?php get_permalink($program) ?>"><?php echo get_the_title($program); ?></a></li>
+      <?php }
+      echo '</ul>';
+      }
+
+    ?>
+
   </div>
 
 <?php endwhile; ?>
